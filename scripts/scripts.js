f03db005bd97a8d20a8166551b1438e97152dba5
Maged Eltorkoman <newmaged@gmail.com>
2022-11-10 11:01:00 +0200

    Initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,16 @@
+const output = document.getElementById("output");
+output.style.visibility = "hidden";
+
+
+// TODO listen to input and output in grams and pounds and ounces
+
+document.getElementById("kgInput").addEventListener("input", function(event){
+    output.style.visibility = "visible";
+    let kg = event.target.value;
+    document.getElementById("gmOutput").innerHTML = kg * 1000;
+    document.getElementById("lbOutput").innerHTML = kg * 2.2;
+    document.getElementById("ozOutput").innerHTML = kg * 35.27;
+
+})
+   
+
